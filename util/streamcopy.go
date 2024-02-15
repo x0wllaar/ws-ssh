0864e9d62668cd3ecad38641e12304e7f7c73382
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-15 18:27:12 -0500

    fix segfault on nil errors

--- util/streamcopy.go
+++ util/streamcopy.go
@@ -13,14 +13,22 @@ func StreamCopy(logger *slog.Logger, from io.ReadWriter, to io.ReadWriter) error
 	go func() {
 		logger.Debug("Starting to -> from copy")
 		n, err := io.Copy(from, to)
-		logger.Debug("To -> from copy ended", slog.Int64("copied", n), slog.String("error", err.Error()))
+		if err != nil {
+			logger.Debug("To -> from copy ended", slog.Int64("copied", n), slog.String("error", err.Error()))
+		} else {
+			logger.Debug("To -> from copy ended", slog.Int64("copied", n))
+		}
 		tfChan <- err
 	}()
 
 	go func() {
 		logger.Debug("Starting from -> to copy")
 		n, err := io.Copy(to, from)
-		logger.Debug("To -> from copy ended", slog.Int64("copied", n), slog.String("error", err.Error()))
+		if err != nil {
+			logger.Debug("From -> to copy ended", slog.Int64("copied", n), slog.String("error", err.Error()))
+		} else {
+			logger.Debug("From -> to copy ended", slog.Int64("copied", n))
+		}
 		ftChan <- err
 	}()
 
