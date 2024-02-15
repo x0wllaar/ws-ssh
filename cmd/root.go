33c7b46267df36408782c86c9b396867107801b1
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-15 18:49:03 -0500

    add better help for root command

--- cmd/root.go
+++ cmd/root.go
@@ -36,8 +36,18 @@ run
 	ws-ssh listen --from 127.0.0.1:8822 --to 127.0.0.1:22
 then add to nginx config:
 	location /ws-ssh {
-    	proxy_pass  http://127.0.0.1:8822/;
+		proxy_pass http://127.0.0.1:8822/;
+		proxy_http_version 1.1;
+		proxy_set_header Upgrade $http_upgrade;
+		proxy_set_header Connection "Upgrade";
+		proxy_set_header Host $host;
 	}
+and restart nginx for the changes to take effect
+
+It's also recommended to add frequent SSH keepalives to such connections:
+	Host yoursite.com
+		ServerAliveInterval 10
+		ServerAliveCountMax 2
 
 On the client:
 	ssh -o ProxyCommand="ws-ssh connect --url https://yoursite.com/ws-ssh stdio" yoursite.com
