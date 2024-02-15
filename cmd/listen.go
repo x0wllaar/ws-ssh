c9901df1755e9499171845ba9c95f88cc1f6bca4
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-15 16:31:20 -0500

    add cobra cli skeleton

---
+++ cmd/listen.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2024 Grigorii Khvatskii <gkhvatsk@nd.edu>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// listenCmd represents the listen command
+var listenCmd = &cobra.Command{
+	Use:   "listen",
+	Short: "Listen for incoming websocket connections and forward them to a TCP port",
+	Long: `This command starts a process that listens for incoming websocket connections
+and forwards them to a TCP port on this or on another server.
+
+For example:
+	ws-ssh listen --from tcp4:127.0.0.1:8822 --to tcp4:127.0.0.1:22
+will listen for incoming websocket connections on http://127.0.0.1/8822 and
+forward them to 127.0.0.1:22, enabling ssh connections over websockets`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("listen called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listenCmd)
+
+	listenCmd.Flags().StringP("from", "f", "tcp4:127.0.0.1:8822", "Where to listen for incoming connections")
+	listenCmd.Flags().StringP("to", "t", "tcp4:127.0.0.1:22", "Where to forward the connections")
+}
---
+++ cmd/stdio.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2024 Grigorii Khvatskii <gkhvatsk@nd.edu>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// stdioCmd represents the stdio command
+var stdioCmd = &cobra.Command{
+	Use:   "stdio",
+	Short: "Forward stdio to a websocket connection",
+	Long: `Connects to a websocket server, then copies bytes from stdin to
+the connection and from the connection to stdout.
+
+This can be useful for the ProxyCommand option in SSH.
+
+To use:
+	ws-ssh connect stdio --url https://yoursite.com/ws-ssh
+To use with SSH:
+	ssh -o ProxyCommand="ws-ssh connect --url https://yoursite.com/ws-ssh stdio" yoursite.com
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("stdio called")
+	},
+}
+
+func init() {
+	connectCmd.AddCommand(stdioCmd)
+}
