0b113f0ce564bc0c53f296c648c61f1e6c2cd1ee
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-18 03:08:23 -0500

    move connect command implementation to separate file

--- impl/client/client.go
+++ impl/client/client.go
@@ -16,38 +16,9 @@ limitations under the License.
 package client
 
 import (
-	"context"
-	"fmt"
-	"io"
 	"log/slog"
 	"os"
-
-	"grisha.xyz/ws-ssh/util"
-	"nhooyr.io/websocket"
 )
-
-func connectCmdImpl(logger *slog.Logger, url string, from io.ReadWriter) error {
-	localLogger := logger.With(slog.String("to", url))
-	localLogger.Info("Starting cleint")
-
-	websockRaw, _, err := websocket.Dial(context.Background(), url, nil)
-	if err != nil {
-		localLogger.Error("Error connecting to websocket", slog.String("error", err.Error()))
-		return fmt.Errorf("error connecting to websocket: %w", err)
-	}
-	defer websockRaw.CloseNow()
-
-	websock := websocket.NetConn(context.Background(), websockRaw, websocket.MessageBinary)
-	defer websock.Close()
-
-	err = util.StreamCopy(localLogger, from, websock)
-	if err != nil {
-		localLogger.Warn("Errors when copying streams", slog.String("error", err.Error()))
-	}
-
-	slog.Info("Done copying streams")
-	return nil
-}
 
 func ConnectCmdImplStdIo(logger *slog.Logger, url string) error {
 	logger.Info("Using stdio")
---
+++ impl/client/connectcmdimpl.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2024 Grigorii Khvatskii <gkhvatsk@nd.edu>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package client
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+
+	"grisha.xyz/ws-ssh/util"
+	"nhooyr.io/websocket"
+)
+
+func connectCmdImpl(logger *slog.Logger, url string, from io.ReadWriter) error {
+	localLogger := logger.With(slog.String("to", url))
+	localLogger.Info("Starting cleint")
+
+	websockRaw, _, err := websocket.Dial(context.Background(), url, nil)
+	if err != nil {
+		localLogger.Error("Error connecting to websocket", slog.String("error", err.Error()))
+		return fmt.Errorf("error connecting to websocket: %w", err)
+	}
+	defer websockRaw.CloseNow()
+
+	websock := websocket.NetConn(context.Background(), websockRaw, websocket.MessageBinary)
+	defer websock.Close()
+
+	err = util.StreamCopy(localLogger, from, websock)
+	if err != nil {
+		localLogger.Warn("Errors when copying streams", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Done copying streams")
+	return nil
+}
