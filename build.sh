a30a977a3fc8ab892d3c3f6b911ec67dbdf3e9d6
Grigorii Khvatskii <gkhvatsk@nd.edu>
2024-02-20 16:35:12 -0500

    move everything cli-related into its own package

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 set -exuo pipefail
 
+cp ./browserproxy/build/rootpage.html ./impl/wsclient/browserproxy/rootpage.gen.html
+pushd ./impl/wsclient/browserproxy/
+./generate_fake_rootpage.sh
+popd
+
+pushd ./browserproxy
+./build_rootpage.sh 
+popd
+
+cp ./browserproxy/build/rootpage.html ./impl/wsclient/browserproxy/rootpage.gen.html
+pushd ./impl/wsclient/browserproxy/
+./generate_rootpage.sh
+popd
+
 go build -o ./ws-ssh ./cli
+
